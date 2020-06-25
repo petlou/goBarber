3971ac4ee815c9a3f403f25ca44f96292770ff26
Peter Lourenço <lourenco.pet@gmail.com>
2020-06-25 19:57:03 -0300

    Criando Tooltip de Erros

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -24,7 +24,7 @@ const SignUp: React.FC = () => {
         email: Yup.string()
           .required('Email obrigatório')
           .email('Digite um e-mail válido'),
-        password: Yup.string().min(6, 'Mínimo de 6 caracteres'),
+        password: Yup.string().min(6, 'Mínimo de 6 dígitos'),
       });
 
       await schema.validate(data, {
